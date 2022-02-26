a6eed6c567253f2288301d3d33b05339f64b7429
Tomás Figueiredo <dev.tomas2003@gmail.com>
2022-02-26 16:56:07 +0000

    Traduções e outras alterações

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -28,17 +28,64 @@ import { AiOutlineClose } from 'react-icons/ai';
 import Security from '../../components/Security';
 import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
+import { sha256 } from 'js-sha256';
 
 export default function Home(){
+    type langProps = {
+        linkLang: String,
+        menuProc: String,
+        menuCards: String,
+        menuSec: String,
+        menuMails: String,
+        authCanUser: String,
+        authSysError: String,
+        userUnauth: String,
+        unauthMessage: String,
+        unauthBtn: String
+    }
+
     const [showUnauth, setShowUnauth] = useState<Boolean>(true);
-    const [loginError, setLoginError] = useState<String>('');
+    const [loginErrorSys, setLoginErrorSys] = useState<Boolean>(false);
+    const [loginErrorUsr, setLoginErrorUsr] = useState<Boolean>(false);
     const [background] = useState<Number>(Math.floor(Math.random() * (2 - 1 + 1) + 1));
     const [nome, setNome] = useState<String>('');
+    const [langList, setLangList] = useState<langProps>();
+    const [lang, setLang] = useState<String>('en');
 
+    async function changeLang(){
+        if(lang === "pt"){
+            const langRes = await api.get('/getLang/pt');
+            setLangList(langRes.data);
+            setLang(langRes.data.linkLang);
+        }else{
+            const langRes = await api.get('/getLang/en');
+            setLangList(langRes.data);
+            setLang(langRes.data.linkLang);
+        }
+    }
+    function getParam(q: string) {
+        return (window.location.search.match(new RegExp('[?&]' + q + '=([^&]+)')) || [, null])[1];
+    }
     useEffect(function(){
-        function testAuth(){
+        async function testAuth(){
+            if(getParam("lang") !== null){
+                if(getParam("lang") === "en"){
+                    const langRes = await api.get('/getLang/en');
+                    setLangList(langRes.data);
+                    setLang(langRes.data.linkLang);
+                }else{
+                    const langRes = await api.get('/getLang/pt');
+                    setLangList(langRes.data);
+                }
+            }else{
+                const langRes = await api.get('/getLang/pt');
+                setLangList(langRes.data);
+            }
             if(sessionStorage.getItem("authToken") === null && sessionStorage.getItem("authSession") !== null){
-                api.get('/checkOAuth/' + sessionStorage.getItem("authSession")).then(function(res){
+                api.post('/checkOAuth', {
+                    "authSession": sessionStorage.getItem("authSession"),
+                    "navigatorFinPrt": sha256(window.screen.availHeight + window.screen.availWidth + window.screen.colorDepth + window.screen.pixelDepth + navigator.appVersion + navigator.appCodeName + navigator.appName + navigator.language + navigator.platform + navigator.product + navigator.productSub + navigator.vendor)
+                }).then(function(res){
                     if(res.data.status === "ok"){
                         sessionStorage.setItem("authToken", res.data.token);
                         sessionStorage.removeItem("authSession");
@@ -46,11 +93,11 @@ export default function Home(){
                         setShowUnauth(false);
                     }else{
                         sessionStorage.removeItem("authSession");        
-                        setLoginError("Operação de autenticação cancelada pelo utilizador!");
+                        setLoginErrorUsr(true);
                     }
                 }).catch(() => {
                     sessionStorage.removeItem("authSession");
-                    setLoginError("Ocorreu um erro interno no serviço de autenticação!");
+                    setLoginErrorSys(true);
                 });
             }else if(sessionStorage.getItem("authToken") !== null && sessionStorage.getItem("authSession") === null){
                 api.get('/validateJwt/', {
@@ -66,7 +113,7 @@ export default function Home(){
                     }
                 }).catch(() => {
                     sessionStorage.removeItem("authToken");
-                    setLoginError("Ocorreu um erro interno no serviço de autenticação!");
+                    setLoginErrorSys(true);
                 });
             }
         }
@@ -82,23 +129,33 @@ export default function Home(){
             if(res.data.status !== "error"){
                 setNome(res.data.amigavel);
             }else{
-                alert("erro");
+                sessionStorage.removeItem("authToken");
+                setShowUnauth(true);
+                setLoginErrorSys(true);
             }
         }).catch(() => {
-            alert("erro");
+            sessionStorage.removeItem("authToken");
+            setShowUnauth(true);
+            setLoginErrorSys(true);
         });
     }
 
     function startAuth(){
-        api.get('/getExternalAuthToken').then(function(res){
+        api.post('/getExternalAuthToken', {
+            "navigatorFinger": sha256(window.screen.availHeight + window.screen.availWidth + window.screen.colorDepth + window.screen.pixelDepth + navigator.appVersion + navigator.appCodeName + navigator.appName + navigator.language + navigator.platform + navigator.product + navigator.productSub + navigator.vendor)
+        }).then(function(res){
             if(res.data.status === "ok"){
-                location.href = "http://127.0.0.1:3000/login?authSession=" + res.data.token;
+                if(lang === "en"){
+                    location.href = "http://127.0.0.1:3000/login?authSession=" + res.data.token + "&lang=pt";
+                }else{
+                    location.href = "http://127.0.0.1:3000/login?authSession=" + res.data.token + "&lang=en";
+                }
                 sessionStorage.setItem("authSession", res.data.token);
             }else{
-                setLoginError("Ocorreu um erro interno no serviço de autenticação!");
+                setLoginErrorSys(true);
             }
         }).catch(() => {
-            setLoginError("Ocorreu um erro interno no serviço de autenticação!");
+            setLoginErrorSys(true);
         });
     }
 
@@ -114,7 +171,7 @@ export default function Home(){
                         <>
                             <UserName>{nome}</UserName>
                             <BoxSideTopInfo>
-                                <LangChange>en</LangChange>
+                                <LangChange onClick={() => {changeLang()}}>{langList?.linkLang}</LangChange>
                                 <BtnExit>
                                     <ImExit size={18} color="#fff" />
                                 </BtnExit>
@@ -122,18 +179,26 @@ export default function Home(){
                         </>
                         :
                         <>
-                        <UserName>Utilizador não autenticado</UserName>
+                        <UserName>{langList?.userUnauth}</UserName>
                         <BoxSideTopInfo>
-                            <LangChange>en</LangChange>
+                            <LangChange onClick={() => {changeLang()}}>{langList?.linkLang}</LangChange>
                         </BoxSideTopInfo>
                         </>
                         }
                     </DataAuthInfo>
                 </UserAuthInfo>
-                { loginError !== "" ?
+                { loginErrorSys ?
                 <ErrorLine>
-                    <ErrorText>{loginError}</ErrorText>
-                    <BtnCloseError onClick={() => {setLoginError('')}}>
+                    <ErrorText>{langList?.authSysError}</ErrorText>
+                    <BtnCloseError onClick={() => {setLoginErrorSys(false)}}>
+                        <AiOutlineClose size={18} color="#721c24" />
+                    </BtnCloseError>
+                </ErrorLine>
+                : null }
+                { loginErrorUsr ?
+                <ErrorLine>
+                    <ErrorText>{langList?.authCanUser}</ErrorText>
+                    <BtnCloseError onClick={() => {setLoginErrorUsr(false)}}>
                         <AiOutlineClose size={18} color="#721c24" />
                     </BtnCloseError>
                 </ErrorLine>
@@ -141,10 +206,10 @@ export default function Home(){
                 { !showUnauth ?
                 <ContentPage>
                     <MenuBox>
-                        <MenuItem>O meu processo</MenuItem>
-                        <MenuItem>Os meus cartões de identificação</MenuItem>
-                        <MenuItem>Segurança</MenuItem>
-                        <MenuItem>Lista de e-mails</MenuItem>
+                        <MenuItem>{langList?.menuProc}</MenuItem>
+                        <MenuItem>{langList?.menuCards}</MenuItem>
+                        <MenuItem>{langList?.menuSec}</MenuItem>
+                        <MenuItem>{langList?.menuMails}</MenuItem>
                     </MenuBox>
                     <ContainerBox>
                         <Security />
@@ -154,9 +219,9 @@ export default function Home(){
                 <ContentPage>
                     <UnauthBox>
                         <FaUserShield size={200} color="#444" />
-                        <UnauthText>Inicie sessão para aceder ao myAETTR</UnauthText>
+                        <UnauthText>{langList?.unauthMessage}</UnauthText>
                         <BtnLogin onClick={() => {startAuth()}}>
-                            <LoginText>Iniciar Sessão</LoginText>
+                            <LoginText>{langList?.unauthBtn}</LoginText>
                         </BtnLogin>
                     </UnauthBox>
                 </ContentPage>
